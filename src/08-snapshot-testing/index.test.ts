a20aeaba757946696af3d4c5770e5877c9bcb34d
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-02-12 18:29:02 +0100

    feat: add snapsjot tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,29 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elements = [1, 'ggg', undefined];
+    const expectedLinkedList = {
+      value: 1,
+      next: {
+        value: 'ggg',
+        next: {
+          value: null,
+          next: {
+            value: null,
+            next: null,
+          },
+        }
+      }
+    }
+    expect(generateLinkedList(elements)).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const linkedList = generateLinkedList([1, 'ff'])
+    expect(linkedList).toMatchSnapshot();
   });
 });
